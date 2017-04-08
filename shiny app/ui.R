a711f268a7aded30d32622585f39ab5fd335da80
Alexander Guldbrand <alexguldbrand@gmail.com>
2017-04-08 11:59:30 +0200

    'I gang med exercise 2'

--- shiny app/ui.R
+++ shiny app/ui.R
@@ -5,17 +5,24 @@ ui <- shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       h2('Menu'),
-      img(src='petal.jpg', height = 100, width = 100)
+      br(),
+      h4('ActionButton'),
+      actionButton('Per', label = 'Perform'),
+      br(),
+      h4('Submitbutton'),
+      submitButton('Submit')
     ),
     mainPanel(
       h1('Main'),
+      br(),
+      br(),
+      img(src='petal.jpg', height = 150, width = 200),
       p('This famous (Fisher’s or Anderson’s)', a('iris data', href = 'http://stat.ethz.ch/R-manual/R-devel/library/datasets/html/iris.html'), 'set gives the measurements in 
         centimeters of the variables sepal length and width and petal length and width, 
         respectively, for 50 flowers from each of 3 species of iris. The species are', 
         strong('Iris setosa,'), strong('versicolor'), 'and', strong('virginica'),'.'),
       br(),
       h2('Analysis')
-      
     )
   )
 ))
