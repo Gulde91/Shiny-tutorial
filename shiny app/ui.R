7021f4f8d49efe03e37bb3d24dcfe7e0ae4d94ca
Alexander Guldbrand <alexguldbrand@gmail.com>
2017-04-08 12:32:26 +0200

    'Færdig med Exercise 2'

--- shiny app/ui.R
+++ shiny app/ui.R
@@ -10,7 +10,14 @@ ui <- shinyUI(fluidPage(
       actionButton('Per', label = 'Perform'),
       br(),
       h4('Submitbutton'),
-      submitButton('Submit')
+      submitButton('Submit'),
+      h4('Single Checkbox'),
+      checkboxInput('checkbox', label='Choice A', value = F),
+      checkboxGroupInput('checkGroup', label = h4('Checkbox Group'), 
+                         choices = list('valg 1' = 1, 'valg 2' = 2, 'valg 3' = 3), selected = 2),
+      dateInput('date', label = h4('Date input'), value = '2016-12-01'),
+      dateRangeInput('dates', label = h4('Date Ranges'), start = '2000-01-01', format = 'd/M/yy'),
+      fileInput('file', label = h4('File input'))
     ),
     mainPanel(
       h1('Main'),
