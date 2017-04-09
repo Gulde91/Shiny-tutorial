8717b2efef463de35fa4ad6eaf786b8a512084d7
Alexander Guldbrand <alexguldbrand@gmail.com>
2017-04-09 17:32:40 +0200

    færdig med exercise 9

--- shiny app/dashboard.R
+++ shiny app/dashboard.R
@@ -3,7 +3,39 @@ library(shinydashboard)
 
 ui <- dashboardPage(
 # Header 
-      dashboardHeader(title = 'Shiny app'),
+      dashboardHeader(title = 'Shiny app',
+                      dropdownMenuOutput('messageMenu'),
+                      dropdownMenu(type = 'notifications',
+                                   notificationItem(
+                                     text = "20 new users today",
+                                     icon("users")
+                                   ),
+                                   notificationItem(
+                                     text = "14 items delivered",
+                                     icon("truck"),
+                                     status = "success"
+                                   ),
+                                   notificationItem(
+                                     text = "Server load at 84%",
+                                     icon = icon("exclamation-triangle"),
+                                     status = "warning"
+                                   )
+                                   ),
+                      dropdownMenu(type = 'tasks', badgeStatus = 'success',
+                                   taskItem(value = 100, color = "green",
+                                            "Documentation"
+                                            ),
+                                   taskItem(value = 17, color = "aqua",
+                                            "Project X"
+                                            ),
+                                   taskItem(value = 75, color = "yellow",
+                                            "Server deployment"
+                                            ),
+                                   taskItem(value = 80, color = "red",
+                                            "Overall project"
+                                            )
+                                   )
+                      ),
 # Sidebar      
       dashboardSidebar(
         sidebarMenu(
@@ -16,29 +48,31 @@ ui <- dashboardPage(
       dashboardBody(
         tabItems(
           tabItem(tabName = 'dt', 
-                  h2('Iris Data table'),
+                  h2('Data table'),
                   fluidRow(
-                    box(dataTableOutput('Table'), width = 400)
+                    box(title = 'Iris data', solidHeader = T, status = 'primary',
+                        dataTableOutput('Table'), width = 400)
                   )
                   ),
           tabItem(tabName = 'sm', 
                   h2('Summary'),
                   fluidRow(
-                    box(dataTableOutput('Table2'), width = 400)
+                    box(title = 'Summary af Iris data', solidHeader = T, status = 'primary',
+                        dataTableOutput('Table2'), width = 400)
                   )
                   ),
           tabItem(tabName = 'km', 
                   h2('K-means'),
                   fluidRow(
-                    box(plotOutput('plot1', click = 'mouse')),
-                    box(sliderInput('slider1', label = h4('Clusters'), 
+                    box(title = 'Scatterplot', solidHeader = T, status = 'primary', plotOutput( 'plot1', click = 'mouse')),
+                    box(status = 'warning', sliderInput('slider1', label = h4('Clusters'), 
                                     min = 1, max = 9, value = 4),
                         verbatimTextOutput('coord'))
                   ),
                   fluidRow(
-                    box(checkboxGroupInput('checkGroup', label = h4('Variable X'), 
+                    box(status = 'warning', checkboxGroupInput('checkGroup', label = h4('Variable X'), 
                                            names(iris), selected = names(iris)[[1]])),
-                    box(selectInput('select', label = h4('Variable Y'), names(iris), 
+                    box(status = 'warning', selectInput('select', label = h4('Variable Y'), names(iris), 
                                     selected = names(iris)[[2]]))
                   )
                   )
@@ -81,7 +115,31 @@ server <- shinyServer(function(input, output) {
     kmeans(Data(), input$slider1)
   })
   
+  output$messageMenu <- renderMenu({
+    # Code to generate each of the messageItems here, in a list. messageData
+    # is a data frame with two columns, 'from' and 'message'.
+    # Also add on slider value to the message content, so that messages update.
+    msgs <- apply(messageData, 1, function(row) {
+      messageItem(
+        from = row[["from"]],
+        message = paste(row[["message"]], input$slider)
+      )
+    })
+    dropdownMenu(type = "messages", .list = msgs)
+  })
+  
 })
+
+messageData <- data.frame(
+  from = c("Admininstrator", "New User", "Support"),
+  message = c(
+    "Sales are steady this month.",
+    "How do I register?",
+    "The new server is ready."
+  ),
+  stringsAsFactors = FALSE
+)
+
 
 shinyApp(ui=ui, server = server)
 
